6c8d49af06a9a868aa8e1a5c2d65b3b8ad72aee4
Teck Cheng Lee <tlee@c1r3s6.42singapore.sg>
2025-07-31 18:05:08 +0800

    add item and remove item in cart

--- raid-swe/src/components/FruitList.tsx
+++ raid-swe/src/components/FruitList.tsx
@@ -1,13 +1,20 @@
-import { fruits } from '../data/fruits';
+import { Fruit, fruits } from '../data/fruits';
 
-export function FruitList() {
+interface FruitListProps {
+	fruits: Fruit[];
+	onAddToCart: (fruit: Fruit) => void;
+}
+
+export function FruitList({fruits, onAddToCart} :FruitListProps) {
 	return (
 		<div>
 			<h2>Available Fruits</h2>
 			<ul>
 				{fruits.map((fruit) => (
 					<li key={fruit.id}>
-						{fruit.name} - ${fruit.price.toFixed(2)} (Stock: {fruit.stock})
+						{fruit.name} - ${fruit.price.toFixed(2)} (Stock: {fruit.stock}){" "}
+						<button onClick={() => onAddToCart(fruit)} disabled={fruit.stock <= 0}>
+							Add to Cart</button>
 					</li>
 				))}
 			</ul>
