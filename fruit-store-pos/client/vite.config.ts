3bac9da511146a70f6ae6958ddff8b152b06f345
teckchenglee <teckchenglee@hotmail.com>
2025-08-05 23:53:02 +0800

    Fix

--- fruit-store-pos/client/vite.config.ts
+++ fruit-store-pos/client/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig, preprocessCSS } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/raid-swe-challenge"
 })
